9c3efff406e04cc8fcb4c1f6f52c7ee3f35b5e79
shahalammirza <mshahalam1246@gmail.comm>
2026-02-24 04:24:52 +0600

    implement interview and reject status toggle with a render

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // step 1 total cards count
 let interviewList = [];
 let rejectList = [];
+let currentStatus = 'all';
 
 let totalCount = document.getElementById("total-count");
 let interviewCount = document.getElementById("interview-count");
@@ -10,7 +11,7 @@ const allCards = document.getElementById("all_cards");
 
 function calculateCount(){
     totalCount.innerText = allCards.children.length;
-    interviewCount.innerText = rejectList.length;
+    interviewCount.innerText = interviewList.length;
     rejectCount.innerText = rejectList.length;
 }
 calculateCount();
@@ -32,10 +33,27 @@ function filterStyleBtn(id){
     rejectFilterBtn.classList.add("bg-white", "border", "border-[#F1F2F4]", "text-[#64748B]");
 
     const selected = document.getElementById(id);
+    currentStatus = id;
     selected.classList.add("bg-[#3B82F6]", "text-white")
     selected.classList.remove("bg-white");
+
+    if(id === 'interview-filter-btn'){
+        allCards.classList.add('hidden');
+        filteredAllCards.classList.remove('hidden');
+        renderingInterview();
+    }else if(id == 'all-filter-btn'){
+        allCards.classList.remove('hidden');
+        filteredAllCards.classList.add('hidden');
+    }else if(id === 'reject-filter-btn'){
+        allCards.classList.add('hidden');
+        filteredAllCards.classList.remove('hidden');
+        renderingReject();
+    }
+
+    calculateCount();
 }
 
+console.log(interviewList)
 // step 3 catch the data form the main container
 const mainContainer = document.querySelector('main');
 mainContainer.addEventListener('click', function(event){
@@ -62,7 +80,122 @@ mainContainer.addEventListener('click', function(event){
         if(!interviewExistItem){
             interviewList.push(cardInfo);
         }
+
+        rejectList = rejectList.filter(item => item.companyName != cardInfo.companyName);
+        if(currentStatus === 'reject-filter-btn'){
+            renderingReject();
+        }
+        calculateCount();
+    }
+    else if(event.target.classList.contains('reject_btn')){
+        const parenNode = event.target.parentNode.parentNode;
+        const companyName = parenNode.querySelector('.company_name').innerText;
+        const positionName = parenNode.querySelector('.position_name').innerText;
+        const jobTypeSalary = parenNode.querySelector('.jobType_salary').innerText;
+        const status = parenNode.querySelector('.status').innerText;
+        const description = parenNode.querySelector('.description').innerText;
+
+        parenNode.querySelector('.status').innerText = "REJECTED";
+
+        const cardInfo = {
+            companyName,
+            positionName,
+            jobTypeSalary,
+            status: "REJECTED",
+            description
+        }
+
+        const rejectExistItem = rejectList.find(item => item.companyName == cardInfo.companyName);
+
+        if(!rejectExistItem){
+            rejectList.push(cardInfo);
+        }
+
+        interviewList = interviewList.filter(item => item.companyName != cardInfo.companyName);
+        if(currentStatus === 'interview-filter-btn'){
+           renderingInterview();
+        }
+        calculateCount();
     }
 
 
 })
+
+// step 4 rendering interview list array information by index ways
+const filteredAllCards = document.getElementById('filter_all_cards');
+
+// interview rendering
+function renderingInterview(){
+    filteredAllCards.innerHTML = '';
+
+    for (let interview of interviewList){
+        const div = document.createElement('div');
+        div.className = 'card_wrap bg-white p-[24px] border border-[#F1F2F4] rounded-[8px] mt-[16px] flex justify-between';
+
+        div.innerHTML = `
+             <!-- card-left -->
+            <div class="card_left">
+                <!-- step1 -->
+                <p class="company_name text-[18px] text-[#002C5C] font-semibold">Mobile First Corp</p>
+                <!-- step2 -->
+                <p class="position_name text-[16px] text-[#64748B]">React Native Developer</p>
+                <!-- step3 -->
+                <p class="jobType_salary text-[14px] text-[#64748B] mt-[20px]">Remote • Full-time • $130,000 - $175,000</p>
+                <!-- step4 -->
+                <p class="status font-medium text-[14px] text-[#323B49] uppercase px-[12px] py-[8px] bg-[#EEF4FF] inline-block mt-[20px]">Not Applied</p>
+                <!-- step5 -->
+                <p class="description text-[14px] text-[#323B49] mt-[8px]">Build cross-platform mobile applications using React Native. Work on products used by millions of users worldwide.</p>
+                <!-- step6 -->
+                <div class="card_btn flex gap-[8px] mt-[20px]">
+                    <button class="interview_btn text-[14px] font-bold uppercase px-[12px] py-[8px] rounded-[4px] border border-[#10B981] text-[#10B981] cursor-pointer">interview</button>
+                    <button class="reject_btn text-[14px] font-bold uppercase px-[12px] py-[8px] rounded-[4px] border border-[#EF4444] text-[#EF4444] cursor-pointer">Rejected</button>
+                </div>
+            </div>
+
+            <!-- card right -->
+            <div class="card_right">
+                <button class="delete_btn h-[32px] w-[32px] border border-[#F1F2F4] rounded-full flex justify-center items-center cursor-pointer"><img src="img/del.png" alt=""></button>
+            </div>
+        `
+        filteredAllCards.appendChild(div)
+    }
+
+}
+
+// reject rendering
+function renderingReject(){
+    filteredAllCards.innerHTML = '';
+
+    for (let reject of rejectList){
+        const div = document.createElement('div');
+        div.className = 'card_wrap bg-white p-[24px] border border-[#F1F2F4] rounded-[8px] mt-[16px] flex justify-between';
+
+        div.innerHTML = `
+             <!-- card-left -->
+            <div class="card_left">
+                <!-- step1 -->
+                <p class="company_name text-[18px] text-[#002C5C] font-semibold">Mobile First Corp</p>
+                <!-- step2 -->
+                <p class="position_name text-[16px] text-[#64748B]">React Native Developer</p>
+                <!-- step3 -->
+                <p class="jobType_salary text-[14px] text-[#64748B] mt-[20px]">Remote • Full-time • $130,000 - $175,000</p>
+                <!-- step4 -->
+                <p class="status font-medium text-[14px] text-[#323B49] uppercase px-[12px] py-[8px] bg-[#EEF4FF] inline-block mt-[20px]">Not Applied</p>
+                <!-- step5 -->
+                <p class="description text-[14px] text-[#323B49] mt-[8px]">Build cross-platform mobile applications using React Native. Work on products used by millions of users worldwide.</p>
+                <!-- step6 -->
+                <div class="card_btn flex gap-[8px] mt-[20px]">
+                    <button class="interview_btn text-[14px] font-bold uppercase px-[12px] py-[8px] rounded-[4px] border border-[#10B981] text-[#10B981] cursor-pointer">interview</button>
+                    <button class="reject_btn text-[14px] font-bold uppercase px-[12px] py-[8px] rounded-[4px] border border-[#EF4444] text-[#EF4444] cursor-pointer">Rejected</button>
+                </div>
+            </div>
+
+            <!-- card right -->
+            <div class="card_right">
+                <button class="delete_btn h-[32px] w-[32px] border border-[#F1F2F4] rounded-full flex justify-center items-center cursor-pointer"><img src="img/del.png" alt=""></button>
+            </div>
+        `
+        filteredAllCards.appendChild(div)
+    }
+
+}

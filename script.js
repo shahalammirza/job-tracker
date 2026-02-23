358ecde96eb1ee5fc2f12b6312ca4c4879e2f384
shahalammirza <mshahalam1246@gmail.comm>
2026-02-24 03:03:50 +0600

    catch the id for count tabs cards and creat function for calculation and creat array for pushing interview and reject cards information

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("hello world")
+// step 1 total cards count
+let interviewList = [];
+let rejectList = [];
+
+let totalCount = document.getElementById("total-count");
+let interviewCount = document.getElementById("interview-count");
+let rejectCount = document.getElementById("reject-count");
+
+const allCards = document.getElementById("all_cards");
+
+function calculateCount(){
+    totalCount.innerText = allCards.children.length;
+    interviewCount.innerText = rejectList.length;
+    rejectCount.innerText = rejectList.length;
+}
+calculateCount();
